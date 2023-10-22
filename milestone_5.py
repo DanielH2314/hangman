fcff2d26733d2ab027470a59c8fc143fe21e1356
DanielH2314 <danielwhuddart@gmail.com>
2023-10-22 19:59:18 +0100

    adds docstrings to file

--- milestone_5.py
+++ milestone_5.py
@@ -12,6 +12,15 @@ class Hangman:
         self.list_of_guesses = []
     
     def check_guess(self, guess):
+        """
+        This function checks if the letter guessed is in the word.
+        
+        This function first checks that the letter is found in the word.
+        If so, it informs the player this is the case.
+        It then replaces the '_' in the word being guessed with the correspoding letter.
+        After this, the number of letters left to guess is reduced by one.
+        If the letter is not in the word then the player is informed and they lose a life.
+        """
         guess.isalpha()
         if str(guess) in self.word:
             print(f"Good guess! {guess} is in the word.")
@@ -26,6 +35,14 @@ class Hangman:
             
 
     def ask_for_input(self):
+        """
+        This function asks the user the guess a letter.
+        
+        The player enters a letter. The function first checks if this is a single character and a letter in the alphabet.
+        If this is not the case the player is informed.
+        If the letter is a single character then the function is ran to check whether this letter is in the word.
+        Finally the function adds the letter to the list of guessed letters, ensuring the player can only guess a specific letter once. 
+        """
         guess = input("Guess a letter: ")
         if guess.isalpha() == False or len(guess) != 1:
             print("Invalid letter. Please, enter a single alphabetical character.")
@@ -37,6 +54,15 @@ class Hangman:
                 
 
 def play_game(word_list):
+    """
+    This function plays a game of Hangman 
+    
+    This is a function that chooses a random word from the list passed to the function and starts a game with five lives.
+    The function displays how many characters are in the word and asks the player to continuously guess a letter. 
+    They are told if the letter is in the word or not. Each time they guess incorrectly they lose a life.
+    If the player runs out of lives the game finishes. 
+    If the player guesses all the characters, they win the game.
+    """
     game = Hangman(word_list = word_list, num_lives = 5)
     print(game.word_guessed)
     while True:
